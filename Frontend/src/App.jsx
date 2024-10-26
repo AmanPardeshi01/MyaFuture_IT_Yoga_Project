8a3676d0ea8d0c4939ef16a61b539842d08a5df4
AmanPardeshi01 <amanpardeshi540@gmail.com>
2024-10-26 06:05:18 +0530

    "Updated Frontend code: refactored Header component, added new components (TestimonialSection, Footer, Gallery), and rearranged component order in HomePage."

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,17 +3,26 @@ import { Route, Routes } from 'react-router-dom'
 import AuthLayout from './components/auth/layout'
 import AuthLogin from './pages/auth/login'
 import AuthRegister from './pages/auth/register'
+import Header from './components/Client/Header/Header'
+import HomePage from './components/Client/HomePage'
 
 function App() {
 
   return (
-    <div className='flex flex-col overflow-hidden bg-white'>
-      { /* Common */ }
-      <h1>Header Component</h1>
+    <div >
+      { /* Common */}
+      {/* <h1>Header Component</h1> */}
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}>
+        {/* <Route path="/auth" element={<AuthLayout />}>
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
+        </Route> */}
+        <Route path="/header" element={<Header />}>
+
+        </Route>
+
+        <Route path="/" element={<HomePage />}>
+
         </Route>
       </Routes>
 
---
+++ Frontend/src/components/Client/HomePage/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../Header/Header'
+import ContactUs from '../ContactSection/ContactUs'
+import CardSection from '../CardSection/CardSection'
+
+const HomePage = () => {
+    return (
+
+            <div>
+                <Header />
+                <ContactUs />
+                <CardSection/>
+            </div>
+       
+
+    )
+}
+
+export default HomePage
